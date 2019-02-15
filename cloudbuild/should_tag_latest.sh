4245fa9119c3edcabcd264b272f0b12f39f6567e
Huy Huynh <huyhuynh@google.com>
2019-02-15 14:54:05 -0800

    Fix not having access to both gcloud and docker

--- cloudbuild/should_tag_latest.sh
+++ cloudbuild/should_tag_latest.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Determine the appropriate image to tag "latest".
+# Determines if the "latest" tag should be on the local tag.
+# Outputs "true or false".
+#
 # It pulls down the tags that are currently available remotely.
 # Among those and the new local tag, the highest version is picked
 # to be tagged as "latest".
@@ -14,7 +16,7 @@
 set -eu
 
 readonly repo="$1"
-readonly tag="$2"
+readonly local_tag="$2"
 
 latest_remote_version=$( \
   gcloud container images list-tags "$repo" \
@@ -23,16 +25,17 @@ latest_remote_version=$( \
     | egrep '[0-9]+\.[0-9]+\.[0-9]+' \
     | sort -t '.' -k 1,1 -k 2,2 -k 3,3 -g \
     | tail -n 1)
-echo "Latest remote version: $latest_remote_version"
+echo "Latest remote version: $latest_remote_version" >&2
 latest_version=$( \
-  printf '%s\n%s' "$latest_remote_version" "$tag" \
+  printf '%s\n%s' "$latest_remote_version" "$local_tag" \
     | sort -t '.' -k 1,1 -k 2,2 -k 3,3 -g \
     | tail -n 1)
-echo "Latest version should be: $latest_version"
+echo "Latest version should be: $latest_version" >&2
 
 # If the tag is not the local tag, we need to pull it down
 # before tagging "latest" locally.
-if [[ "$tag" != "$latest_version" ]]; then
-  docker pull "$repo:$latest_version"
+if [[ "$local_tag" != "$latest_version" ]]; then
+  echo "false"
+else
+  echo "true"
 fi
-docker tag "$repo:$latest_version" "$repo:latest"
